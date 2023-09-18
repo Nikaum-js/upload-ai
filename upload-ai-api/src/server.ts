0119e8ab2dc4b83d6fc9fa920f054e11e6f9f98c
Nikolas Santana de Arruda <narruda.ext@mtrix.com.br>
2023-09-18 13:50:12 -0300

    Preparando o deploy :tada:

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -1,10 +1,9 @@
-import { fastifyCors } from '@fastify/cors'
+import fastifyCors from '@fastify/cors'
 import { fastify } from 'fastify'
 import { createTranscriptionRoute } from './routes/create-transcription'
 import { generateAICompletionRoute } from './routes/generate-ai-completion'
 import { getAllPromptsRoute } from './routes/get-all-prompts'
-import { getVideosRoute } from './routes/get-videos'
-import { uploadVideoRoute } from './routes/upload-video'
+import { postUploadVideoRoute } from './routes/upload-video'
 
 const app = fastify()
 
@@ -12,9 +11,8 @@ app.register(fastifyCors, {
   origin: '*'
 })
 
-app.register(getVideosRoute)
-app.register(uploadVideoRoute)
 app.register(getAllPromptsRoute)
+app.register(postUploadVideoRoute)
 app.register(createTranscriptionRoute)
 app.register(generateAICompletionRoute)
 
